3a77a899cfd0dbe42aa03f873442da2ea49db1d8
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-07-05 18:59:11 -0300

    Add Button
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="custom-button"> SOBREVIVA! </button>
+</template>
+
+<script>
+export default {
+    name: "Button"
+}
+</script>
+
+<style scoped>
+.custom-button {
+  background: linear-gradient(to right, #ff7f00, #00bfff);
+  color: white;
+  padding: 12px 24px;
+  border-width: 5px;
+  border-radius: 4px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+.custom-button:hover {
+  background: linear-gradient(to right, #ff7f00, #00bfff);
+  background-size: 200% auto;
+  background-position: right center;
+}
+
+.custom-button:focus {
+  outline: none;
+}
+</style>
