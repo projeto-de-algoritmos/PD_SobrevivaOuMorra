1679ed86961152483d1eeae9e2d78083c1e33077
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-07-05 19:01:34 -0300

    add view Result
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <div class="master">
+    <Navbar />
+    <router-view/>
+  </div>
 </template>
-
 
 <script>
 import Navbar from './components/Navbar.vue';
@@ -13,3 +14,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.master {
+  position: absolute;
+  background-color: gray;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;  
+  height: 100%;
+}
+
+</style>
---
+++ src/views/Result.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>RESULT!</h1>
+    <div>
+        Morreu? Viveu?
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'Result',
+}
+
+</script>
