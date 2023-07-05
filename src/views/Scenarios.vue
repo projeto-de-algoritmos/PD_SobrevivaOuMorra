72bc656e17f1cf20b2ddada000acb7acf21cbf6a
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-07-05 19:00:34 -0300

    add view scenarios
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

---
+++ src/views/Scenarios.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="scenario">
+    <h1>Escolha o seu desafio!</h1>
+    <div class="casa" @click="$router.push('/houseFire')" ></div>
+>
+
+  </div>
+</template>
+
+<script>
+
+import Button from "./../components/Button"
+
+export default {
+  name: "Scenario",
+}
+</script>
+
+<style scoped>
+
+h1{
+  top: 10%;
+  left: 50%;
+  font-size:50px;
+}
+
+.scenario{
+  background-color: gold;
+  background-image: url(../assets/escolhas.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;  
+  height: 100%;
+  
+}
+
+.casa{
+  margin: 5%;
+  background-image: url(../assets/casaFogo.png);
+  background-size: cover;
+  background-repeat: no-repeat;  width: 20%;  
+  height: 20%;
+  border-radius: 40px;
+}
+
+</style>
+
+
+
