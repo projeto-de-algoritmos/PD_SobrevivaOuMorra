c04e3542b6fa48c6649764f8fc48de10bed40c74
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-07-06 12:21:50 -0300

    add scenarios
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- src/views/Scenarios.vue
+++ src/views/Scenarios.vue
@@ -1,22 +1,43 @@
 <template>
   <div class="scenario">
     <h1>Escolha o seu desafio!</h1>
-    <div class="casa" @click="$router.push('/houseFire')" ></div>
->
-
+    <div class="scenario-list">
+      <div class="scenario-item" @click="$router.push('/houseFire')">
+        <div class="icon casa"></div>
+        <span>Casa em chamas</span>
+      </div>
+      <div class="scenario-item">
+        <div class="icon fimMundo"></div>
+        <span>Fim do mundo</span>
+      </div>
+      <div class="scenario-item">
+        <div class="icon apocalipseZumbi"></div>
+        <span>Apocalipse zumbi</span>
+      </div>
+      <div class="scenario-item">
+        <div class="icon desastreNatural"></div>
+        <span>Desastre natural</span>
+      </div>
+    </div>
+    <Button @click="$router.push('/')" :buttonName="nomeBotao"/>
   </div>
 </template>
 
 <script>
-
 import Button from "./../components/Button"
+
 
 export default {
   name: "Scenario",
+  components: {
+    Button,
+  },
+  data(){
+    return {
+      nomeBotao:"Voltar",
+    }
+  } 
 }
 </script>
 
-<style src="../css/Scenarios.css"></style>
-
-
-
+<style src="../css/Scenarios.css" scoped></style>
