f08af6cccb1c711a426ea3010f2d4ecf66d327b8
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-07-06 12:20:39 -0300

    Remove nav bar
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="master">
-    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
 
 export default {
-  components:{
-    Navbar,
-  }
 }
 </script>
 
